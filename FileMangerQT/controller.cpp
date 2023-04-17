4591493c33f2991f5757dc4248b990cc033a6b5b
Ziad Fahmy <68162606+zyadfahmy2@users.noreply.github.com>
2023-04-17 21:53:39 +0200

    modified paste,delete
    1- paste function now supports to  paste an entire directory recursively
    2-delete function can delete an entire directory
    3-TODO: paste directory should be in a separate thread

--- FileMangerQT/controller.cpp
+++ FileMangerQT/controller.cpp
@@ -7,38 +7,59 @@ Controller::Controller(View *view)
 
 void Controller::mRegisterSignals()
 {
-    QObject::connect(dView, &View::copyFile, this, &Controller::copyFile);
+    QObject::connect(dView, &View::copyFile, this, &Controller::paste);
     QObject::connect(dView, &View::delFile, this, &Controller::delFile);
     QObject::connect(dView, &View::cutFile, this, &Controller::cutFile);
 }
 
-void Controller::copyFile(fs::path source_path, fs::path destination_path, CopyCutAction action)
+void Controller::paste(fs::path source_path, fs::path destination_path, CopyCutAction action)
 {
     if(action == CopyCutAction::Copy)
     {
         try {
-            destination_path = destination_path / source_path.filename();
+            fs::path pathObj(source_path);
+            std::string directoryName = pathObj.filename().string();
+            destination_path = destination_path / directoryName;
             // Check if source file exists
             if (!fs::exists(source_path)) {
                 qInfo() << "Source file does not exist!\n" ;
                 return ;
             }
-            std::cout << destination_path;
-            // Check if destination file exists
-            if (fs::exists(destination_path)) {
-                qInfo() << "Destination file already exists!\n";
-                return ;
+            if (fs::is_directory(source_path))
+            {
+//                if (!fs::create_directory(destination_path)) {
+//                    throw std::runtime_error("Failed to create destination directory");
+//                }
+                try {
+                    fs::copy(source_path, destination_path, fs::copy_options::recursive);
+                }
+                catch (std::exception& e) { // Not using fs::filesystem_error since std::bad_alloc can throw too.
+                    // Handle exception or use error code overload of fs::copy.
+                }
+
+            }
+            else
+            {
+                destination_path = destination_path / source_path.filename();
+                std::cout << destination_path;
+                // Check if destination file exists
+                if (fs::exists(destination_path)) {
+                    qInfo() << "Destination file already exists!\n";
+                    return ;
+                }
+
+                // Copy the file
+                fs::copy_file(source_path, destination_path);
+
+                std::cout << destination_path;
+                qInfo() << "File copied successfully.\n";
             }
 
-            // Copy the file
-            fs::copy_file(source_path, destination_path);
-
-            std::cout << destination_path;
-            qInfo() << "File copied successfully.\n";
             return ;
         }
         catch (const std::exception& ex) {
-            qInfo() << "Error: " ;
+            qInfo() << "Error: "  ;
+            std::cout<<destination_path;
         }
     }
     else if(action == CopyCutAction::Cut)
@@ -61,9 +82,22 @@ void Controller::delFile(boost::filesystem::path filePath)
     }
     try
     {
-        // Remove the file
-        fs::remove(filePath);
-        qInfo() << "File deleted successfully!";
+        if (fs::is_directory(filePath))
+        {
+            try {
+                fs::remove_all(filePath);
+                qInfo() << "directory deleted successfully!";
+            } catch (const std::exception& ex) {
+                std::cerr << "Error deleting directory: " << ex.what() << std::endl;
+            }
+        }
+        else
+        {
+            fs::remove(filePath);
+            qInfo() << "File deleted successfully!";
+        }
+
+
     }
     catch (fs::filesystem_error const &e)
     {
--- FileMangerQT/controller.h
+++ FileMangerQT/controller.h
@@ -4,6 +4,8 @@
 #include <QObject>
 #include "view.h"
 #include <boost/filesystem.hpp>
+#include <boost/filesystem/operations.hpp>
+#include <filesystem>
 #include <QDebug>
 #include <iostream>
 namespace fs = boost::filesystem;
@@ -20,7 +22,7 @@ private:
 
     void mRegisterSignals();
 public slots:
-    void copyFile(fs::path source_path, fs::path destination_path, CopyCutAction action);
+    void paste(fs::path source_path, fs::path destination_path, CopyCutAction action);
     void delFile(fs::path filePath);
     void cutFile(const boost::filesystem::path& path);
     void renameFile(const boost::filesystem::path &path , const std::string newFileName);
