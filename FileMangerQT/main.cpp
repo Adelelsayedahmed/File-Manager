2f0ec6ef765c7f8d0515e76363104dc6da917af3
Ziad Fahmy <68162606+zyadfahmy2@users.noreply.github.com>
2023-05-05 05:09:33 +0300

    added a theme

--- FileMangerQT/main.cpp
+++ FileMangerQT/main.cpp
@@ -1,10 +1,20 @@
 #include "view.h"
 #include "controller.h"
 #include <QApplication>
-
+#include <QLatin1String>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QFile styleSheetFile("/home/ziad/Documents/GitHub/File-Manager/FileMangerQT/QSS-master/MacOS.qss");
+
+    if (!styleSheetFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        // Handle the error if the file cannot be opened
+        qDebug() << "Failed to open file:" ;
+    }
+
+    QString stylesheet = QLatin1String(styleSheetFile.readAll()) ;
+    qInfo()<<stylesheet;
+    a.setStyleSheet(stylesheet);;
     View w;
     Controller c(&w);
     w.show();
